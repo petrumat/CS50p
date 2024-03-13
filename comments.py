34123dc652b95e9de8992155f57ab3fb61cb3e8c
PetruMat <petru.mateescu@stud.acs.upb.ro>
2024-03-13 21:52:53 +0200

    add comments

---
+++ comments.py
@@ -0,0 +1,13 @@
+# This is a single line comment
+
+# This is
+# a multiline
+# comment
+
+'''
+This is a multiline comment as well.
+
+In this program nothing is performed,
+
+just some comments are showed.
+'''
--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("hello, world")
+# Printing just a string variable (with default new line at end) 
+test_var = "This is just a test"
+print(test_var)
+
+# Printing a string variable without new line
+
+
+# Input string from user and prints it
+name = input("What's your name? ")
+print(f"hello, {name}")
