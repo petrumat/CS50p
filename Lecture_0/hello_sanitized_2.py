aa25e4029d8b1de906feceb692ee6c63dff33e5f
PetruMat <petru.mateescu@stud.acs.upb.ro>
2024-03-14 14:03:57 +0200

    create Lecture_0 folder

--- Lecture_0/hello_sanitized_2.py
+++ Lecture_0/hello_sanitized_2.py
@@ -1,14 +1,11 @@
-# Set maximum number of tries
-MAX_TRY = 3
-
 # Ask user for input
 name = input("What's your name? ")
 
 # Remove leading and trailing special characters, digits, separators and symbols
 name = name.strip(" 1234567890.,<>/?\\|;:\'\"[]{}-=_+*!@#$%^&()\t")
 
-# Capitalize name
-name = name.capitalize()
+# Capitalize each word in name
+name = name.title()
 
 # Greet user name
 print(f"hello, {name}")
